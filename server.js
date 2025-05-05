af71bb3fac27d88085aae397fb130097e1996e38
MohamedELbohy1 <elbohym33@gmail.com>
2025-05-05 19:09:36 +0200

    Modify-Datebase

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD,
+// );
 
 //mongoose.connect(process.env.DATABASE_LOCAL,{
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:false
-}).then(()=> console.log('DB connestions Successful'));
-
+mongoose
+  .connect(process.env.DATABASE)
+  .then((con) => {
+    console.log('DB Connected Succssefully');
+  })
+  .catch((err) => {
+    console.log(`There was and error ${err}`);
+    process.exit(1);
+  });
 
 //console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on Port ${port}`);
+  console.log(`App running on Port ${port}`);
 });
 
+process.on('uncaughtException', (err) => {
+  console.log(`uncaughtException error: ${err.name}|${err.message}`);
+  server.close(() => {
+    console.log('Shutting Down The Server....');
+    process.exit(1);
+  });
+});
